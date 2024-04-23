5dada72fa842b68f2a53a43601f589716a604f7b
Christian <97773844+esuriru@users.noreply.github.com>
2024-04-23 13:27:33 +0800

    Add comments to `FlickeringLight`

--- Assets/Scripts/Light/FlickeringLight.cs
+++ Assets/Scripts/Light/FlickeringLight.cs
@@ -1,3 +1,4 @@
+// NOTE - Remove superfluous usings
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,22 +8,27 @@ public class FlickeringLight : MonoBehaviour
 {
     [SerializeField] private VisualEffect _visualEffect;
     [SerializeField] private Light _light;
+
+    // NOTE - These public variables can be made private
     public float multiplier;
     public float fadeSpeed;
 
     private bool started = false;
 
+    // NOTE - Missing access specifier 
     void Awake()
     {
         _visualEffect = GetComponentInParent<VisualEffect>();
         _light = GetComponent<Light>();
     }
 
+    // NOTE - Missing access specifier 
     void OnEnable()
     {
         started = false;
     }
 
+    // NOTE - Missing access specifier 
     void Update()
     {
         _light.intensity = Random.Range(1, 3) * multiplier;
